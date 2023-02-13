120092fb43f9c5b1b7ff147b5e68d3b7416324f3
sumit <123495189+Sumitdongre8799@users.noreply.github.com>
2023-02-13 23:31:43 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+
+//  Q(1). Write a simple string program to take input from user ?
+
+import java.util.*;
+
+public class string {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name :");
+        String str = sc.nextLine();
+        System.out.println("Welldone ! " +str + " your program is successfully run : " );
+    }
+}
