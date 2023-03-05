36c394d2e5e5569f84147b01d55fadcc3081cd44
sumit <123495189+Sumitdongre8799@users.noreply.github.com>
2023-03-05 22:00:19 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class anagram {
+    public static void main(String args[]) {
+
+        String str1 = "School Master";
+        String str2 = "The Classroom";
+        str1 = str1.replace("", "");
+        str2 = str2.replace("", "");
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        char[] ar1 = str1.toCharArray();
+        char[] ar2 = str2.toCharArray();
+
+        Arrays.sort(ar1);
+        Arrays.sort(ar2);
+
+        if (Arrays.equals(ar1, ar2)) {
+            System.out.println("it is Anagram");
+        } else {
+            System.out.println("it is not Anagram");
+        }
+
+    }
+}
